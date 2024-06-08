1614aa660950ac4b94cdda52481f074340c54327
bcsbs2112333@szabist.pk <bcsbs2112333@szabist.pk>
2024-06-08 17:46:33 +0500

    FP Camera Tweaks By Haris

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,22 +4,37 @@ public class CameraController : MonoBehaviour
 {       
     [SerializeField] Transform player;
     [SerializeField] private float rotationSpeed = 2f;
-    
+
     [SerializeField] float minVerticalAngle = -20;
     [SerializeField] float maxVerticalAngle = 45;
 
     [SerializeField] float headHeight = 1.8f; // Adjust this value to match the height of the player's head
-    
+
     [SerializeField] bool invertX;
     [SerializeField] bool invertY;
-    
+
     private float invertXVal;
     private float invertYVal;
 
     float rotationY;
     float rotationX;
-    
-    // Start is called before the first frame update
+
+    public float Sensitivity {
+        get { return sensitivity; }
+        set { sensitivity = value; }
+    }
+
+    [Range(0.1f, 9f)]
+    [SerializeField] float sensitivity = 2f;
+
+    [Tooltip("Limits vertical camera rotation. Prevents the flipping that happens when rotation goes above 90.")]
+    [Range(0f, 90f)]
+    [SerializeField] float yRotationLimit = 88f;
+
+    Vector2 rotation = Vector2.zero;
+    const string xAxis = "Mouse X"; //Strings in direct code generate garbage, storing and re-using them creates no garbage
+    const string yAxis = "Mouse Y";
+
     void Start()
     {
         // Initially position the camera at the player's head position
@@ -27,26 +42,26 @@ public class CameraController : MonoBehaviour
         transform.rotation = player.transform.rotation;
     }
 
-    // Update is called once per frame
     void Update()
     {
         invertXVal = invertX ? -1 : 1;
         invertYVal = invertY ? -1 : 1;
-        
-        rotationY += Input.GetAxis("Mouse X") * invertXVal * rotationSpeed;
-        rotationX += Input.GetAxis("Mouse Y") * -invertYVal * rotationSpeed;
 
-        rotationX = Mathf.Clamp(rotationX, minVerticalAngle, maxVerticalAngle);
+        rotation.x += Input.GetAxis(xAxis) * sensitivity * invertXVal;
+        rotation.y += Input.GetAxis(yAxis) * -sensitivity * -invertYVal;
+        rotation.y = Mathf.Clamp(rotation.y, -yRotationLimit, yRotationLimit);
 
-        var targetRotation = Quaternion.Euler(rotationX, rotationY, 0);
+        var xQuat = Quaternion.AngleAxis(rotation.x - 40f, Vector3.up);
+        var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
 
-        // Apply the rotation to the camera and the player
-        transform.rotation = targetRotation;
-        player.rotation = Quaternion.Euler(0, rotationY, 0);
+        transform.localRotation = xQuat * yQuat;
 
         // Ensure the camera follows the player's head position
         transform.position = player.position + Vector3.up * headHeight;
+
+        // Apply horizontal rotation to the player
+        player.rotation = Quaternion.Euler(0, rotation.x, 0);
     }
-    
-    public Quaternion PlanarRotation => Quaternion.Euler(0, rotationY, 0);
+
+    public Quaternion PlanarRotation => Quaternion.Euler(0, rotation.x, 0);
 }
